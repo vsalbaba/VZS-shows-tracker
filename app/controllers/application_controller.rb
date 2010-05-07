744ce27eb31ff5615bb66b3f222cdb0b89722207
Vojtech Salbaba <vojta@vojta-laptop.(none)>
2010-05-07 21:22:05 +0200

    pridani napovedy pro Textile v cestine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,31 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  def textile_help
+    @list_example = %{
+* prvni polozka
+* druha polozka
+** treti, vnorena polozka
+
+# cislovana polozka
+#* vnorena polozka, ale ne cislovana
+# druha cislovana polozka
+}
+
+    @emphasis_example = %{
+Text který chcete zvýraznit napište mezi *hvězdičky* nebo _podtržítka_.
+
+*duležité, tedy tučné a upoutávající pozornost při pohledu na text*
+_kurzíva, upoutávající pozornost při čtení_
+}
+
+
+    @links_example = %{
+Odkaz se píše v uvozovkách a kam se uživatel dostane za dvojtečku za uvozovky.
+"Toto bude text odkazu":http://example.com
+}
+
+    render 'shared/textile_help'
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ ActionController::Routing::Routes.draw do |map|
   map.logout 'logout', :controller => 'sessions', :action => 'destroy'
   map.login 'login', :controller => 'sessions', :action => 'new'
 
+  map.textile_help 'help', :controller => 'application', :action => 'textile_help'
+
   map.resources :sessions
 end
 
