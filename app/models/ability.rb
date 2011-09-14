b0650533d38acae6981434caea0da9f0e200490c
Vojtech Salbaba <darktatka@gmail.com>
2011-09-14 12:55:49 +0200

    oprava pri neprihlasenem uzivateli

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,21 +4,23 @@ class Ability
 
   def initialize(user)
     can :feed, Show
-    if user.admin?
-      can :see_subscription, Subscription
-      can :subscribe_others, Show
-      can :manage, :all
-      cannot :delete, Show
-    elsif user.superadmin?
-      can :see_subscription, Subscription
-      can :manage, :all
-      can :subscribe_others, Show
-    else
-      cannot :index, :stats
-      cannot :kick, Show
-      cannot :unarchive, Show
-      cannot :archive, Show
-      cannot :subscribe_others, Show
+    if user
+      if user.admin?
+        can :see_subscription, Subscription
+        can :subscribe_others, Show
+        can :manage, :all
+        cannot :delete, Show
+      elsif user.superadmin?
+        can :see_subscription, Subscription
+        can :manage, :all
+        can :subscribe_others, Show
+      else
+        cannot :index, :stats
+        cannot :kick, Show
+        cannot :unarchive, Show
+        cannot :archive, Show
+        cannot :subscribe_others, Show
+      end
     end
   end
 end
