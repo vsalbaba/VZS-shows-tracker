4fc3eb811f869afebf19cb9e48cbe1881e058121
Vojtech Salbaba <darktatka@gmail.com>
2011-09-15 12:00:17 +0200

    filtrovani zaznamu vsech uzivatelu

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,7 +3,14 @@
 class StatisticsController < ApplicationController
   before_filter :login_required
   def stats
-    @statistics = User.all.map {|user| Statistic.new user}
+    @from_date = parse_date(params[:from_date])
+    @to_date = parse_date(params[:to_date]) || Date.current
+    @statistics = User.all.map {|user| Statistic.new user, :from => @from_date, :to => @to_date}
+  end
+
+  private
+  def parse_date datestring
+     Date.strptime(datestring, '%d. %m. %Y') if datestring.present?
   end
 end
 
